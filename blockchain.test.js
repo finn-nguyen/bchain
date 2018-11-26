98fe66708cf2e46db40614745a6f62acf556b556
nvtuongcs <nvtuongcs@gmail.com>
2018-11-26 22:53:55 +0700

    Add chain validation, replace with a new chain

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,9 +2,10 @@ const Blockchain = require('./blockchain')
 const Block = require('./block')
 
 describe('Blockchain', () => {
-  let bc
+  let bc, bc2
   beforeEach(() => {
     bc = new Blockchain()
+    bc2 = new Blockchain()
   })
 
   it('start with genesis block', () => {
@@ -15,5 +16,33 @@ describe('Blockchain', () => {
     const data = 'foo'
     bc.addBlock(data)
     expect(bc.chain[bc.chain.length - 1].data).toEqual(data)
+  })
+
+  it('validates a valid chain', () => {
+    bc2.addBlock('foo')
+    expect(bc.isValidChain(bc2.chain)).toBe(true)
+  })
+
+  it('validates a chain with a corrupt genesis block', () => {
+    bc2.chain[0].data = 'Wrong data'
+    expect(bc.isValidChain(bc2.chain)).toBe(false)
+  })
+
+  it('validates a corrupt chain', () => {
+    bc2.addBlock('foo')
+    bc2.chain[1].data = 'Wrong data'
+    expect(bc.isValidChain(bc2.chain)).toBe(false)
+  })
+
+  it('replaces the chain with a valid chain', () => {
+    bc2.addBlock('goo')
+    bc.replaceChain(bc2.chain)
+    expect(bc.chain).toEqual(bc2.chain)
+  })
+
+  it('does not replace the chain with one of less then or equal length', () => {
+    bc.addBlock('foo')
+    bc.replaceChain(bc2.chain)
+    expect(bc.chain).not.toEqual(bc2.chain)
   })
 })
